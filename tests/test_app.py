0ca0297b368a60ee70db82831a1a4a88d7943c64
shelldeveloper <mz15@uchicago.edu>
2025-08-14 19:40:32 -0500

    fixed test_app.py #2

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,6 @@
 # Run the tests with: pytest tests/test_app.py
 from ..app import add_numbers
 
-"""Test that add_numbers correctly sums two numbers"""
 def test_add_numbers():
+    """Test that add_numbers correctly sums two numbers"""
     assert 3 == add_numbers(1,2)
