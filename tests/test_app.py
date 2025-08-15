4da07ca104f3c6195a930ecc4cce5e082752172d
shelldeveloper <mz15@uchicago.edu>
2025-08-14 19:33:31 -0500

    fixed test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,8 @@
+"""Tests for the say_hi app"""
+
 # Run the tests with: pytest tests/test_app.py
 from ..app import add_numbers
 
+"""Test that add_numbers correctly sums two numbers"""
 def test_add_numbers():
     assert 3 == add_numbers(1,2)
